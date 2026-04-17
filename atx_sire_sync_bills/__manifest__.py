384e49dae28dda98423222b53f65afedc9a6f8ba
LucianMarquina <alemarquina789@gmail.com>
2026-04-17 15:55:17 -0500

    feat: creation of a basic structure ready for implementation

---
+++ atx_sire_sync_bills/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'ATX SIRE Sync Bills',
+    'author': 'Alessandro Marquina',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Sync vendor bills from SUNAT SIRE into Odoo (Peru)',    
+    'license': 'LGPL-3',
+    'depends': [
+        'accountant',
+        'l10n_pe',
+    ],        
+    'application': True,
+    'auto_install': True,
+}
